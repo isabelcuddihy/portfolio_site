f20494cb425711f5125b55d8eaef5e11144bab73
Isabel Cuddihy <104743014+isabelcuddihy@users.noreply.github.com>
2024-07-01 09:51:16 -0400

    Website fixes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 // src/index.js
 console.log('Hello, Webpack!');
+import './styles.css';
+import './script.js';
+import './slideshow.js';
+
--- webpack.config.js
+++ webpack.config.js
@@ -3,38 +3,40 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js', // adjust entry point if needed
+    entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true,
     },
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
             },
         ],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html', // adjust path if necessary
+            template: './src/index.html',
             filename: 'index.html',
-            chunks: ['main'], // specify which chunks to include if using multiple entry points
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/home.html',
+            filename: 'home.html',
         }),
         new CopyWebpackPlugin({
             patterns: [
-                { from: 'src/*.html', to: '[name].[ext]' }, // copy all HTML files from src folder
-                { from: 'src/*.css', to: '[name].[ext]' }, // copy all CSS files from src folder
-                { from: 'src/*.jpg', to: '[name].[ext]' }, // copy all JPG files from src folder
-                { from: 'src/*.png', to: '[name].[ext]' }, // copy all PNG files from src folder
+                { from: 'src/script.js', to: 'script.js' },
+                { from: 'src/slideshow.js', to: 'slideshow.js' },
             ],
         }),
     ],
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000,
-    },
 };
